c1686f484c4007f78b5fe0be9fa0408c7803ed73
ArtemVichuzhanin <artem.vichuzhanin@gmail.com>
2023-12-27 20:34:22 +0400

    Create project

---
+++ EM_Booking_App/EM_Booking_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EM_Booking_AppApp.swift
+//  EM_Booking_App
+//
+//  Created by Артем  on 27.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct EM_Booking_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
